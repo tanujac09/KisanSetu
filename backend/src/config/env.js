475740b0372ddaa44fed907dc7eac3e3b3b1a5d1
tanujac09 <tanujachavan9.09@gmail.com>
2026-09-07 11:05:07 +0530

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,4 +3,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
 export const JWT_SECRET = process.env.JWT_SECRET || 'kisan-setu-super-secret-change-me';
-export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:5173';
+export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'https://frontend-peach-three-92.vercel.app/';
